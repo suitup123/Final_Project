8186d871863a35dfa9decf7fee96f80e04475e40
Ethan Nguyen <nguyen57595153@gmail.com>
2018-11-02 12:27:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/youma/finalproject/MovieInformation.java
@@ -0,0 +1,13 @@
+package com.example.youma.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MovieInformation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_information);
+    }
+}
